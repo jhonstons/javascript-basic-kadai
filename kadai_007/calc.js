32aed164224b4e3f0098dc3925666853536cb8a5
JTS <151023119+jhonstons@users.noreply.github.com>
2023-12-01 22:41:17 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+// 変数numにランダムな整数を代入する
+let num = Math.floor(Math.random()*100);
+
+// 変数numの値を出力する（確認用）
+console.log(num);
+
+// 変数numの値が15の倍数であれば「3と5の倍数です」文字列を出力する
+if (num %15 === 0) {
+  console.log('3と5の倍数です')
+} 
+else if (num %3 === 0) {
+  console.log('3の倍数です')
+} 
+else if (num %5 === 0) {
+  console.log('5の倍数です')
+}
